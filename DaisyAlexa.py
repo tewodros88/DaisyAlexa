91f073b03faf0d0d17a216bf461072a3326fb8b8
Teddy Mengistu <teddy@wireless-10-104-43-57.umd.edu>
2018-04-17 13:05:14 -0400

    update to flask

--- DaisyAlexa.py
+++ DaisyAlexa.py
@@ -1,9 +1,10 @@
+import logging
 import sys
 import time
 import serial
-
+from random import randint
 from flask import Flask, render_template
-from flask_ask import Ask, statement, question
+from flask_ask import Ask, statement, question, session
 # Import Adafruit IO MQTT client.
 from Adafruit_IO import MQTTClient
 
@@ -21,12 +22,14 @@ ADAFRUIT_IO_USERNAME = 'tewodros'
 def connected(client):
     print('Connected to Adafruit IO!  Listening for Daisy changes...')
     client.subscribe('daisy-call')
+    client.subscribe('daisy-text')
     
 def disconnected(client):
     print('Disconnected from Adafruit IO!')
     sys.exit(1)
 
 def message(client, feed_id, payload):
+    print('Feed {0} received new value: {1}'.format(feed_id, payload))
 
 # Create an MQTT client instance.
 client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
@@ -45,26 +48,72 @@ def welcomemsg():
 
 Team5 = ['teddy', 'Vladimir', 'Jessie']
 
+@ask.intent("MoveIntent")
+def move(direction):
+    if direction == 'left':
+        msg = "moving left"
+    elif direction == 'right':
+        msg = "moving right"
+    elif direction == 'forward':
+        msg = "moving forward"
+    elif direction == 'backward':
+        msg = "moving backward"
+    elif direction == 'move':
+        return question("In what direction?").reprompt("Can you please give a direction?")
+    return question("Moving {}. Can I help you with anything else?".format(direction))
+
 @ask.intent("FollowIntent")
 def follow(firstname):
     if firstname in Team5:
-        msg = "Tracking for {}".format(firstname)
+        msg = "Tracking for {}. Can I help you with anything else?".format(firstname)
     elif firstname == 'follow':
     	return question("Who should I follow?").reprompt("May I please have a name?")
     elif firstname not in Team5:
         msg = "I Can't follow {} he is not a member of Team 5".format(firstname)
         return question(msg).reprompt("May I please have another name?")
+    return question(msg)
+
+@ask.intent("MemoryGameIntent")
+def game():
+
+    numbers = [randint(0, 9) for _ in range(3)]
+    round_msg = render_template('round', numbers=numbers)
+    session.attributes['numbers'] = numbers[::-1]  # reverse
+
+    return question(round_msg)
+
+@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
+
+def answer(first, second, third):
+    winning_numbers = session.attributes['numbers']
+    if [first, second, third] == winning_numbers:
+        msg = render_template('win')
+    else:
+
+        msg = render_template('lose')
     return statement(msg)
 
 @ask.intent("CallIntent")
-def call(firstname):
-	msg = "Hello this is Daisy"
-	client.publish('daisy-call', msg)
+def call():
+    client.publish('daisy-call', 1)
+    return question("Making call, Can I help you with anything else?").reprompt("May I please have a command?")
 
+@ask.intent("TextIntent")
+def call():
+    client.publish('daisy-text', 1)
+    return question("Sending text, Can I help you with anything else?").reprompt("May I please have a command?")
 
+@ask.intent("YesIntent")
+def yes():
+    return question("What would you like to do?").reprompt("May I please have a command?")
 
-while True:
-    time.sleep(10)
+@ask.intent("NoIntent")
+def no():
+    return statement("Ok. goodbye")
 
-if __name__ == "__main__":
-    main()
+@ask.intent("AMAZON.StopIntent")
+def stop():
+    return statement("Stopping")
+    
+if __name__ == '__main__':
+    app.run(debug=True)
--- FlaskAsk.py
+++ FlaskAsk.py
@@ -13,26 +13,6 @@ log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-#ser = serial.Serial('/dev/cu.usbmodem1411', 115200)
-
-def passByte(b):
-    print("Passing byte " + str(b))
-    ser.write(bytes([int(b)]))
-
-def halt():
-    passByte(0)
-
-def moveForward():
-    passByte(1)
-
-def turnRight():
-    passByte(2)
-
-def turnLeft():
-    passByte(3)
-
-def moveBackward():
-    passByte(4)
 
 @ask.launch
 def welcomemsg():
@@ -44,39 +24,42 @@ Team5 = ['teddy', 'Vladimir', 'Jessie']
 @ask.intent("FollowIntent")
 def follow(firstname):
     if firstname in Team5:
-        msg = "Tracking for {}".format(firstname)
+        msg = "Tracking for {}. do you need anymore help".format(firstname)
     elif firstname == 'follow':
     	return question("Who should I follow?").reprompt("May I please have a name?")
     elif firstname not in Team5:
         msg = "I Can't follow {} he is not a member of Team 5".format(firstname)
         return question(msg).reprompt("May I please have another name?")
-    return statement(msg)
-"""
-@ask.intent("WhoToFollowIntent")
-def what_is_my_name():
-    return question("Who should I follow?").reprompt("May I please have a name?")
-"""
+    return question(msg).reprompt("Can I please have a response?")
+
 @ask.intent("MoveIntent")
 def move(direction):
     if direction == 'left':
-        turnLeft()
+        #turnLeft()
         msg = "moving left"
     elif direction == 'right':
-        turnRight()
+        #turnRight()
         msg = "moving right"
     elif direction == 'forward':
-        moveForward()
+        #moveForward()
         msg = "moving forward"
     elif direction == 'backward':
-        moveBackward()
+        #moveBackward()
         msg = "moving backward"
     elif direction == 'move':
         return question("In what direction?").reprompt("Can you please give a direction?")
     return statement(msg)
 
+@ask.intent("YesIntent")
+def yes():
+    return question("How can I help you?").reprompt("Can I please have a response?")
+
+@ask.intent("NoIntent")
+def no():
+    return statement("Ok. Goodbye")
+
 @ask.intent("AMAZON.StopIntent")
 def stop():
-    halt()
     return statement("Stopping")
 
 if __name__ == '__main__':
